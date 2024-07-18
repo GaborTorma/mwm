b2edbacb16f26dd0d1584a35f1cf1e6b03a22e4a
Gábor Torma <gabor@torma.dev>
2024-07-18 18:40:13 +0200

    fix: remove releaseName

--- src/release-it-config/package.ts
+++ src/release-it-config/package.ts
@@ -2,15 +2,14 @@ import defu from 'defu'
 import type { Config } from 'release-it'
 
 export function defineReleaseItConfig(name: string, config?: Config): Config {
-  const releaseName = name.split('/').pop()
   // eslint-disable-next-line no-template-curly-in-string
   const versionTemplate = '${version}'
 
   const defConfig = {
     git: {
-      commitMessage: `Release: ${releaseName} ${versionTemplate}`,
-      tagAnnotation: `Release: ${releaseName} ${versionTemplate}`,
-      tagName: `${releaseName}-v${versionTemplate}`,
+      commitMessage: `Release: ${name} ${versionTemplate}`,
+      tagAnnotation: `Release: ${name} ${versionTemplate}`,
+      tagName: `${name}-v${versionTemplate}`,
       requireCommits: true,
       requireCommitsFail: false,
     },

ef07333d436bbfd2dbb6e536d894858ff4df76ed
Gábor Torma <gabor@torma.dev>
2024-07-19 08:23:42 +0200

    fix: option name for release

--- src/release-it-config/package.ts
+++ src/release-it-config/package.ts
@@ -1,15 +1,23 @@
 import defu from 'defu'
 import type { Config } from 'release-it'
 
-export function defineReleaseItConfig(name: string, config?: Config): Config {
+export function defineReleaseItConfig(name?: string, config?: Config): Config {
   // eslint-disable-next-line no-template-curly-in-string
   const versionTemplate = '${version}'
 
+  const commitMessage = name
+    ? `Release: ${name} ${versionTemplate}`
+    : `Release: ${versionTemplate}`
+
+  const tagName = name
+    ? `${name}-v-${versionTemplate}`
+    : `v-${versionTemplate}`
+
   const defConfig = {
     git: {
-      commitMessage: `Release: ${name} ${versionTemplate}`,
-      tagAnnotation: `Release: ${name} ${versionTemplate}`,
-      tagName: `${name}-v${versionTemplate}`,
+      commitMessage,
+      tagAnnotation: commitMessage,
+      tagName,
       requireCommits: true,
       requireCommitsFail: false,
     },

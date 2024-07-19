7a84a7d1f577c59102a26000fb074d8e5f72dbbf
Gábor Torma <gabor@torma.dev>
2024-07-19 10:12:19 +0200

    fix: export default the release config

--- .release-it.ts
+++ .release-it.ts
@@ -1,5 +1,5 @@
 // eslint-disable-next-line antfu/no-import-dist
-import { defineReleaseItConfig } from './dist/release-it-config/package.cjs'
+import defineReleaseItConfig from './dist/release-it-config/package.cjs'
 import { name } from './package.json'
 
 export default defineReleaseItConfig(name.split('/').pop())

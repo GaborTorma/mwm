e16b6e73d9af81a9181594a326121a0931b0de42
Gábor Torma <gabor@torma.dev>
2024-07-25 16:34:37 +0200

    fix: release issue

--- .release-it.ts
+++ .release-it.ts
@@ -1,5 +1,5 @@
 // eslint-disable-next-line antfu/no-import-dist
-import { defineReleaseItConfig } from './dist/index.cjs'
+import { defineReleaseItConfig } from './dist/release-it-config.cjs'
 import { name } from './package.json'
 
 export default defineReleaseItConfig('release', name.split('/').pop())

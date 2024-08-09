90ebf4d252ef7ddf43061fde5b200c02b38f3e13
Gábor Torma <gabor@torma.dev>
2024-08-09 12:07:39 +0200

    feat: extend package.json replacements

--- src/commands/generate/templates/package.ts
+++ src/commands/generate/templates/package.ts
@@ -2,6 +2,7 @@ import type { Repo } from '../args'
 import type { Replacements } from '../replace'
 
 export function getPackageReplacements(repo: Repo): Replacements {
+  const fullRepo = `${repo.owner.id}/${repo.name}`
   return [
     {
       files: ['package.json'],
@@ -10,12 +11,18 @@ export function getPackageReplacements(repo: Repo): Replacements {
         /(?<="author": ").*(?=")/,
         /(?<="description": ").*(?=")/,
         /(?<="version": ").*(?=")/,
+        /(?<="url": ").*(?=")/,
+        /(?<="homepage": ").*(?=")/,
+        /(?<="bugs": ").*(?=")/,
       ],
       to: [
         `@${repo.owner.id.toLowerCase()}/${repo.name}`,
         repo.owner.name || repo.owner.id,
         repo.description,
         '0.0.1',
+        `git+ssh://git@github.com/${fullRepo}.git`,
+        `https://github.com/${fullRepo}`,
+        `https://github.com/${fullRepo}/issues`,
       ],
     },
   ]

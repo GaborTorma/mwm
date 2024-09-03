53b34dfa967227e5ddc528b62d5a64dfef3af9b3
Gábor Torma <gabor@torma.dev>
2024-09-03 17:28:19 +0200

    fix: addSubmodule for generate

--- src/commands/generate/generate.ts
+++ src/commands/generate/generate.ts
@@ -1,15 +1,15 @@
 import { sleep } from '../../utils/sleep'
-import { cloneRepo } from '../init/git'
 import { pnpmExec } from '../../utils/pnpm'
 import type { Repo } from './args'
 import { type Template, addRemoteTemplate, commitInitChanges, pushChanges } from './templates'
 import { generateGitHubRepo } from './github'
 import { fixFiles, replaceInFiles } from './replace'
+import { addSubmodule } from './git'
 
 export async function generateRepo(template: Template, repo: Repo) {
   await generateGitHubRepo(template, repo)
   await sleep(2000)
-  await cloneRepo(repo)
+  await addSubmodule(repo)
   await addRemoteTemplate(template, repo)
   const replacements = template.getReplacements(repo)
   fixFiles(replacements, repo.path)

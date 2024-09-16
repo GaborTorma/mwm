8e04e3ed2061dcd89694ed4695745b02fea6582e
Gábor Torma <gabor@torma.dev>
2024-09-16 18:03:59 +0200

    feat: more replacement for nuxt-modules

--- src/commands/generate/templates/nuxt-module.ts
+++ src/commands/generate/templates/nuxt-module.ts
@@ -1,6 +1,7 @@
 import type { Template } from '.'
 import type { Repo } from '../args'
 import type { Replacements } from '../replace'
+import { camelCase } from 'change-case'
 import { packageReplacement } from '../replacements/package'
 import { getReadmeReplacement } from '../replacements/readme'
 
@@ -13,6 +14,24 @@ export const nuxtModuleTemplate: Template = {
     return [
       packageReplacement,
       getReadmeReplacement(this, repo),
+      { // configKey
+        type: 'replace',
+        files: [
+          'playground/nuxt.config.ts',
+          'src/module.ts',
+        ],
+        from: new RegExp(camelCase(this.repo), 'g'),
+        to: camelCase(repo.name),
+      },
+      { // module name
+        type: 'replace',
+        files: [
+          'src/module.ts',
+          'src/runtime/plugin.ts',
+        ],
+        from: new RegExp(this.repo, 'g'),
+        to: repo.name,
+      },
     ]
   },
 }

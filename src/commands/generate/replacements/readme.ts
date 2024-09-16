011f45fb7655750a7f4700d7856e7f2eebf8d685
Gábor Torma <gabor@torma.dev>
2024-09-16 18:02:58 +0200

    fix: replace camalCase and pascalCase repo name in README

--- src/commands/generate/replacements/readme.ts
+++ src/commands/generate/replacements/readme.ts
@@ -1,6 +1,7 @@
 import type { Repo } from '../args'
 import type { Replacement } from '../replace'
 import type { Template } from '../templates'
+import { camelCase, pascalCase } from 'change-case'
 
 export function getReadmeReplacement(template: Template, repo: Repo): Replacement {
   return {
@@ -9,13 +10,16 @@ export function getReadmeReplacement(template: Template, repo: Repo): Replacemen
     from: [
       new RegExp(template.owner, 'gi'),
       new RegExp(template.repo, 'g'),
+      new RegExp(camelCase(template.repo), 'g'),
+      new RegExp(pascalCase(template.repo), 'g'),
       '_description_',
     ],
     to: [
       repo.owner.id.toLowerCase(),
       repo.name,
+      camelCase(repo.name),
+      pascalCase(repo.name),
       repo.description,
     ],
-
   }
 }

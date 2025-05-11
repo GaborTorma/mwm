f2bc21c66075e259c23150b057e909e39977695c
Gábor Torma <gabor@torma.dev>
2025-05-11 12:01:57 +0200

    chore: use release-it-config from workspace

--- .release-it.ts
+++ .release-it.ts
@@ -1,5 +1,4 @@
-// eslint-disable-next-line antfu/no-import-dist
-import { defineReleaseItConfig } from './dist/index.cjs'
+import { defineReleaseItConfig } from 'mwm-release-it-config'
 import { name } from './package.json'
 
 export default defineReleaseItConfig('release', name.split('/').pop())
